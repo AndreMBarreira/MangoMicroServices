07ab5f07147da85869544af274b379d1ff85bb7c
AndreMBarreira <andremilla.am@gmail.com>
2024-10-28 17:48:20 -0300

    Login working without JwToken

--- Mango.Services.AuthAPI/Service/IService/IAuthService.cs
+++ Mango.Services.AuthAPI/Service/IService/IAuthService.cs
@@ -5,6 +5,6 @@ namespace Mango.Services.AuthAPI.Service.IService
     public interface IAuthService
     {
         Task<string> Register(RegistrationRequestDto registrationRequestDto);
-        Task<LoginResponseDto> Login(LoginResponseDto loginResponseDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
     }
 }
