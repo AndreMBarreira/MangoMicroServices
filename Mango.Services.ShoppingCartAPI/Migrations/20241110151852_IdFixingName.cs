1baf47fa73ac1263997288e7e56cd121dbc9b80b
AndreMBarreira <andremilla.am@gmail.com>
2024-11-10 14:28:51 -0300

    Cart Upsert Logic EndPoint

---
+++ Mango.Services.ShoppingCartAPI/Migrations/20241110151852_IdFixingName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Services.ShoppingCartAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdFixingName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CartDetailId",
+                table: "CartDetails",
+                newName: "CartDetailsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CartDetailsId",
+                table: "CartDetails",
+                newName: "CartDetailId");
+        }
+    }
+}
