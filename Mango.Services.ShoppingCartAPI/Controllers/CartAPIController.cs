2531ed652c92c5c26c49c0d9f8fae439d1707bec
AndreMBarreira <andremilla.am@gmail.com>
2024-11-15 17:25:31 -0300

    RemoveCart endpoint

--- Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -24,7 +24,7 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
             _mapper = mapper;
         }
 
-        [HttpPost("CartUpsert")]
+            [HttpPost("CartUpsert")]
         public async Task<ResponseDto> CartUpsert(CartDto cartDto)
         {
             try
@@ -66,6 +66,38 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
                     }
                 }
                 _response.Result = cartDto;
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message;
+                _response.isSuccess = false;
+            }
+            return _response;
+
+        }
+
+        [HttpPost("RemoveCart")]
+        public async Task<ResponseDto> RemoveCart([FromBody]int cartDetailsId)
+        {
+            try
+            {
+                CartDetails cartDetails = _db.CartDetails
+                    .First(u => u.CartDetailsId == cartDetailsId);
+                
+                int totalCountfCartItem = _db.CartDetails
+                    .Where(u => u.CartHeaderId == cartDetails.CartHeaderId).Count();
+                _db.CartDetails.Remove(cartDetails);
+
+                if (totalCountfCartItem == 1)
+                {
+                    var cartHeaderToRemove = await _db.CartHeaders
+                        .FirstOrDefaultAsync(u => u.CartHeaderId == cartDetails.CartHeaderId);
+
+                    _db.CartHeaders.Remove(cartHeaderToRemove);
+                }
+                await _db.SaveChangesAsync();
+
+                _response.Result = true;
             }
             catch (Exception ex)
             {
