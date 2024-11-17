cd23e5939596da522fcee19898065f2beeb2efc3
AndreMBarreira <andremilla.am@gmail.com>
2024-11-17 18:58:04 -0300

    WEB Cart begining

---
+++ Mango.Web/Service/IService/ICartService.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICartService
+    {
+        Task<ResponseDto?> GetCartByUserIdAsync(string userId);
+        Task<ResponseDto?> UpsertCartAsync(CartDto cartDto);
+        Task<ResponseDto?> RemoveCartAsync(int cartDetailsId);
+        Task<ResponseDto?> ApplyCouponAsync(CartDto cartDto);
+    }
+}
