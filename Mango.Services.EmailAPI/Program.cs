251c4402f77d91e30dbf50cfec9c060b9464681d
AndreMBarreira <andremilla.am@gmail.com>
2024-12-01 16:26:17 -0300

    Consuming Messages in Action

--- Mango.Services.EmailAPI/Program.cs
+++ Mango.Services.EmailAPI/Program.cs
@@ -1,7 +1,17 @@
+using Mango.Services.EmailAPI.Data;
+using Mango.Services.EmailAPI.Extension;
+using Mango.Services.EmailAPI.Messaging;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddSingleton<IAzureServiceBusConsumer, AzureServiceBusConsumer>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,5 +31,20 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+ApplyMigration();
+app.UseAzureServiceBusConsumer();
 app.Run();
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (_db.Database.GetAppliedMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+
+}
